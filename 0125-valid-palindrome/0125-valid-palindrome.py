3eeb73b57e6fc2eff512f9a083db19c299040a67
amanagantisnigda-prog <amanagantisnigda@gmail.com>
2026-07-27 15:55:16 +0530

    Time: 7 ms (81.04%), Space: 20.4 MB (23.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,17 @@
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         d=""
+#         for i in s:
+#             if i.isalnum():
+#                 d+=i.lower()
+#         if d==d[::-1]:
+#             return True
+#         return False
+
+
+import re
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        d=""
-        for i in s:
-            if i.isalnum():
-                d+=i.lower()
-        if d==d[::-1]:
-            return True
-        return False
+        result = re.sub(r'[^a-zA-Z0-9]','', s).lower()
+        return result == result[::-1]
