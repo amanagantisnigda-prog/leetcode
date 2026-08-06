8fa3d9b49a698ffade08ba1a1ccb204dfb3ff540
amanagantisnigda-prog <amanagantisnigda@gmail.com>
2026-08-06 23:40:47 +0530

    Time: 39 ms (45.12%), Space: 19.4 MB (74.93%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,5 +1,5 @@
-# class Solution:
-#     def longestWord(self, words: List[str]) -> str:
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
         # g={}
         # for i in words:
         #     g[i]=len(i)
@@ -13,22 +13,39 @@
         #         return d
 
 
-class Solution:
-    def longestWord(self, words: List[str]) -> str:
-        wordSet = set(words) # for fast lookup
-        ans = ""
+# class Solution:
+#     def longestWord(self, words: List[str]) -> str:
+#         wordSet = set(words) # for fast lookup
+#         ans = ""
         
-        for word in words:
-            # check all prefixes exist: w, wo, wor, worl for "world"
-            valid = True
-            for i in range(1, len(word)):
-                if word[:i] not in wordSet:
-                    valid = False
-                    break
+#         for word in words:
+#             # check all prefixes exist: w, wo, wor, worl for "world"
+#             valid = True
+#             for i in range(1, len(word)):
+#                 if word[:i] not in wordSet:
+#                     valid = False
+#                     break
             
-            if valid:
-                # condition 1: longer
-                # condition 2: same length but smaller lex order
-                if len(word) > len(ans) or (len(word) == len(ans) and word < ans):
-                    ans = word
-        return ans
+#             if valid:
+#                 # condition 1: longer
+#                 # condition 2: same length but smaller lex order
+#                 if len(word) > len(ans) or (len(word) == len(ans) and word < ans):
+#                     ans = word
+#         return ans
+
+
+
+        visited = set(words)
+        result = ""
+        for word in words:
+            string = ""
+            for w in word:
+                string += w
+                if string not in visited:
+                    break
+            else:
+                if len(word) > len(result):
+                    result = word
+                elif len(word) == len(result):
+                    result = min(result, word)
+        return result
